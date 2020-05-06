2fcf073d06d3905e357cdc8c9dd90e1b6fc1c94a
ris_vb@126.com <ris_vb@126.com>
2020-05-06 13:14:03 +0800

    add experiment032

---
+++ 032.能不能好好说话客户端/032.能不能好好说话客户端/Program.cs
@@ -0,0 +1,29 @@
+﻿using MSXML2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _032.能不能好好说话客户端
+{
+    class Program
+    {
+        //致敬：https://lab.magiconch.com/nbnhhsh/
+
+        public static string SeekSX(string word)
+        {
+            XMLHTTP60 x = new XMLHTTP60();
+            x.open("POST", "https://lab.magiconch.com/api/nbnhhsh/guess");
+            x.setRequestHeader("content-type", "application/json");
+            x.send("{\"text\":\"" + word + "\"}");
+            return x.responseText.Split('[')[2].Split(']')[0];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SeekSX(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
