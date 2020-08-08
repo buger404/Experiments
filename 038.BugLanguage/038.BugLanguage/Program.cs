b7b55a7d06150a4c728f029cebc6ace6db7f8b78
ris_vb@126.com <ris_vb@126.com>
2020-08-08 16:48:31 +0800

    添加火星文生成器

---
+++ 038.BugLanguage/038.BugLanguage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace _038.BugLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        ThreadHead:
+            int y = Console.CursorTop;
+            string ret = BugLanguage.Convert(Console.ReadLine());
+            Console.CursorTop = y;
+            Console.WriteLine("                                                       ");
+            Console.CursorTop = y;
+            Console.WriteLine(ret);
+            Thread.Sleep(100);
+            goto ThreadHead;
+        }
+    }
+}
